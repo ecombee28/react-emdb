e3ab004702ffaa70d8ffce256b4cc518590ef8e0
Eric Combee <45884566+ecombee28@users.noreply.github.com>
2022-06-11 16:41:09 -0700

    fixed bug in cast api

--- src/Components/Cast.jsx
+++ src/Components/Cast.jsx
@@ -9,27 +9,28 @@ const Cast = ({ castMembers }) => {
     <>
       <h1 className={style.title}>Cast </h1>
       <div className={style.cast_wrapper}>
-        {castMembers.map((cast) => (
-          <Link to={`/person/${cast.id}`}>
-            <div className={style.cast_node_wrapper}>
-              <div className={style.image_wrapper}>
-                <img
-                  className={style.cast__img}
-                  src={
-                    cast.profile_path === null
-                      ? BlankImage
-                      : ImagePaths.w500 + cast.profile_path
-                  }
-                  alt="no-profile"
-                />
+        {castMembers.length > 0 &&
+          castMembers.map((cast) => (
+            <Link to={`/person/${cast.id}`}>
+              <div className={style.cast_node_wrapper}>
+                <div className={style.image_wrapper}>
+                  <img
+                    className={style.cast__img}
+                    src={
+                      cast.profile_path === null
+                        ? BlankImage
+                        : ImagePaths.w500 + cast.profile_path
+                    }
+                    alt="no-profile"
+                  />
+                </div>
+                <div className={style.info_wrapper}>
+                  <p className={style.actor_name}>{cast.name}</p>
+                  <p className={style.character_name}>{cast.character}</p>
+                </div>
               </div>
-              <div className={style.info_wrapper}>
-                <p className={style.actor_name}>{cast.name}</p>
-                <p className={style.character_name}>{cast.character}</p>
-              </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
       </div>
     </>
   );
